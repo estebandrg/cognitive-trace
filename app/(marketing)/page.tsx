4362a51b32a658986e7d2c2837631602d7bcf169
estebandrg <esteban.dario.garcia0@gmail.com>
2025-11-13 00:19:55 -0300

    refactor(files): changing files name to kebab-case

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,10 +1,10 @@
 import Navbar from "@/components/marketing-navbar";
-import Hero from "@/components/landing/Hero";
-import FeatureGrid from "@/components/landing/FeatureGrid";
-import ScientificValidation from "@/components/landing/ScientificValidation";
-import HowItWorks from "@/components/landing/HowItWorks";
-import CTA from "@/components/landing/CTA";
-import Footer from "@/components/landing/Footer";
+import Hero from "@/components/landing/hero";
+import FeatureGrid from "@/components/landing/feature-grid";
+import ScientificValidation from "@/components/landing/scientific-validation";
+import HowItWorks from "@/components/landing/how-It-works";
+import CTA from "@/components/landing/cta";
+import Footer from "@/components/landing/footer";
 
 export default function LandingPage() {
   return (
